be01f62df41a333dec6d430a688a2761a193800f
Ezekiel <umanmah234@gmail.com>
2023-02-10 13:10:28 -0500

    implement insertion sort

---
+++ src/InsertionSort.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class InsertionSort {
+    public static int[] insertionSort(int[] arr) {
+//        assume arr[0] is correctly indexed and start at index 1
+        for (int sortedIndex = 1; sortedIndex < arr.length; sortedIndex++) {
+//            capture the value at index 1
+            int curr = arr[sortedIndex];
+//            declare i outside the loop for later reassignment
+            int i;
+//            loop through the sorted partition comparing the current value against values in the sorted partition
+//            right to left. If current value is less than the current sorted partition value, shift the sorted
+//            partition value one index to the right. When the loop condition fails, the correct index is found for
+//            the current value.
+            for(i = sortedIndex; i > 0 && curr < arr[i - 1]; i--){
+                arr[i] = arr[i - 1];
+            }
+            arr[i] = curr;
+        }
+        return arr;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int n = MissingNumber.missingNumber(new int[]{2, 1, 4, 3, 6, 5, 7, 10, 9}, 10); //⇒ 8
@@ -8,5 +10,8 @@ public class Main {
 
         boolean balancedBrackets = BalancedBrackets.isBalanced("Do{ <(you)>}");
         System.out.println("The string has balanced brackets: " + balancedBrackets);
+
+        int[] insertionSort = InsertionSort.insertionSort(new int[]{3, 2, -59, 67});
+        System.out.println(Arrays.toString(insertionSort));
     }
 }
