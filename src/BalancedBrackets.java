cd4598cc4a9d8c3fc23fb9897ad30373ac3c32d0
Ezekiel <umanmah234@gmail.com>
2023-02-10 08:30:08 -0500

    add BalancedBrackets

---
+++ src/BalancedBrackets.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+//  this solution does not account for overlapping brackets e.g. ({ )}
+public class BalancedBrackets {
+    public static boolean isBalanced(String s) {
+        Stack<Character> tracker = new Stack<>();
+        HashMap<Character, Character> brackets = new HashMap<Character, Character>();
+//        because of the LIFO nature of stacks, the example places the closing brackets as the key. This is essential
+//        to the last check for matching bracket types.
+        brackets.put('(', ')');
+        brackets.put('{', '}');
+        brackets.put('<', '>');
+        brackets.put('[', ']');
+
+        for(int i = 0; i < s.length(); i++){
+//            push opening brackets into tracker
+            if(brackets.containsKey(s.charAt(i))){
+                tracker.push(s.charAt(i));
+            } else if (brackets.containsValue(s.charAt(i))) {
+//                immediate return for closing brackets w/o opening brackets
+                if(tracker.isEmpty()) {
+                    return false;
+                }
+
+                Character lastBracket = tracker.pop();
+//                lastBracket will always be an opening bracket; use get() on the hashmap to return the matching
+//                closing bracket. If none of the string chars match, then there is no matching closing bracket
+                if(!brackets.get(lastBracket).equals(s.charAt(i))) {
+                    return false;
+                }
+            }
+        }
+//        there are no brackets opening or closing
+        return tracker.isEmpty();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         int n = MissingNumber.missingNumber(new int[]{2, 1, 4, 3, 6, 5, 7, 10, 9}, 10); //⇒ 8
-        System.out.println(n);
+        System.out.println("The missing number is: " + n);
 
         boolean isBalanced = BalancedParentheses.isBalanced("(Today) we r)ide!");
-        System.out.println(isBalanced);
+        System.out.println("The string has balanced parentheses: " + isBalanced);
+
+        boolean balancedBrackets = BalancedBrackets.isBalanced("Do{ <(you)>}");
+        System.out.println("The string has balanced brackets: " + balancedBrackets);
     }
 }
